12517a8058b9f96893f630c7d4b5a57a5916a5a3
Jeremy Pierre <j.14159@gmail.com>
2013-01-20 11:34:04 -0800

    Small changes to use resolveTerm in comparisons as SExps couldn't be passed in prior. Some comparisons cleanup.

--- src/main/scala/com/noisycode/Comparisons.scala
+++ src/main/scala/com/noisycode/Comparisons.scala
@@ -4,21 +4,11 @@ trait Comparisons {
   this: Evaluator =>
 
   val gt: pf =
-    { case List(Id(">"), a: Value, b: Value) => a > b }//doCompare(List(a, b), ((x, y) => Bool(x > y))) }
+    { case List(Id(">"), a: Term, b: Term) => resolveTerm(a) > resolveTerm(b) }
   val lt: pf =
-    { case List(Id("<"), a: Value, b: Value) => a < b } //doCompare(List(a, b), ((x, y) => Bool(x.n < y.n))) }
+    { case List(Id("<"), a: Value, b: Value) => resolveTerm(a) < resolveTerm(b) }
   val eq: pf =
-    { case List(Id("="), a: Value, b: Value) => a badEq b } //doCompare(List(a, b), ((x, y) => Bool(x.n == y.n))) }
-
-  def doCompare(t: List[Term], m: (AnyVal, AnyVal) => Bool): Term = {
-    t match {
-      case List(a, b) => (resolveTerm(a), resolveTerm(b)) match {
-	case (Number(x), Number(y)) => m(x, y)
-	case (BadChar(x), BadChar(y)) => m(x, y)
-	case _ => Error("Could not resolve on or both to numbers:  " + t.toString)
-      }
-    }
-  }
+    { case List(Id("="), a: Term, b: Term) => resolveTerm(a) badEq resolveTerm(b) }
 
   implicit class ComparableValue(v: Value) {
     def >(v2: Value): Value = {
@@ -35,18 +25,10 @@ trait Comparisons {
 	case (Number(x), Number(y)) => Bool(x < y)
 	case (BadChar(x), BadChar(y)) => Bool(x < y)
 	case (BadString(x), BadString(y)) => Bool(x < y)
-	case (x, y) => Error(s"Can't compare $x and $y with >")
+	case (x, y) => Error(s"Can't compare $x and $y with <")
       }
     }
 
-    def badEq(v2: Value): Value = {
-      (v, v2) match {
-	case (Number(x), Number(y)) => Bool(x == y)
-	case (BadChar(x), BadChar(y)) => Bool(x == y)
-	case (BadString(x), BadString(y)) => Bool(x == y)
-	case (x, y) => Error(s"Can't compare $x and $y with >")
-      }
-    }
-
+    def badEq(v2: Value): Value = Bool(v == v2)
   }
 }
